8cc8072176ee5a47b0ad656e6a204ea2fb72a386
Dominik Włodarz <dwlodarz@pl.sii.eu>
2017-04-28 13:21:45 +0200

    changed the news component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd  } from '@angular/router';
-import { NewsService } from './services/news.service';
-import {NewsModel} from './services/news.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [NewsService]
 })
 export class AppComponent {
   title = 'app works!';
   headerClass = '';
-  news : NewsModel[];
-
-  constructor(private newsService: NewsService, private router: Router) { 
+  
+  constructor(private router: Router) { 
 
   }
 
   ngOnInit() {
-    var a = this.newsService.getAll().subscribe(n=>this.news = n);
+    
     this.router.events.subscribe((val) => {
         if(val instanceof NavigationEnd && (<NavigationEnd> val).url == "" || (<NavigationEnd> val).url=="/")
         {
--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { NewsService } from './../services/news.service';
+import { NewsModel } from './../services/news.model';
 
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
-  styleUrls: ['./news.component.css']
+  styleUrls: ['./news.component.css'],
+  providers: [NewsService]
 })
 export class NewsComponent implements OnInit {
-
-  constructor() { }
+  news: NewsModel[];
+  constructor(private newsService: NewsService) { }
 
   ngOnInit() {
+    this.newsService.getAll().subscribe(n => this.news = n);
   }
-
 }
